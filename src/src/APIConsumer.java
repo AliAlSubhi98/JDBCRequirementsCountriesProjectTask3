868592b9d1d82056d62c6ce628c4530044df4fd1
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-03-21 15:37:11 +0400

    #1 trying to fetch the data from the API (name + common + official)

---
+++ src/src/APIConsumer.java
@@ -0,0 +1,58 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.Gson;
+
+public class APIConsumer {
+	static void showApi() {
+		String apiUrl = "https://restcountries.com/v3.1/all";
+		try {
+			URL url = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			String output;
+			StringBuilder json = new StringBuilder();
+
+			while ((output = br.readLine()) != null) {
+				json.append(output);
+			}
+
+			conn.disconnect();
+
+			Gson gson = new Gson();
+			MyObject myObj[] = gson.fromJson(json.toString(), MyObject[].class);
+
+			// Use myObj for further processing
+			for (MyObject ObjList : myObj) {
+				System.out.println(ObjList.name.common);
+				System.out.println(ObjList.name.official);
+				if (ObjList.name.nativeName != null) {
+					for (String key : ObjList.name.nativeName.keySet()) {
+						System.out.println( key + ": Official: " + ObjList.name.nativeName.get(key).official + " Common: "
+								+ ObjList.name.nativeName.get(key).common);
+					}
+
+					System.out.println("--------------");
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyObject {
+	Name name;
+}
---
+++ src/src/Name.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.util.HashMap;
+
+public class Name {
+String common;
+String official;
+HashMap<String,Language> nativeName ;
+}
+
+class Language{
+	String official;
+	String common;
+}
